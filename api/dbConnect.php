9e2c74eb5082b4499b31e2d2d315fe7967c5de2e
Nicholas <nicholas.moran@selu.edu>
2015-03-17 20:35:25 -0500

    First commit did not have the right code.

--- api/dbConnect.php
+++ api/dbConnect.php
@@ -1,19 +1,20 @@
-$dbhost = "localhost";
+<?php
+
+function getDb()
+{
+	$dbhost = "localhost";
 	$dbuser = "team1";
 	$dbpass = "67yuhjnm^&YUHJNM";
 	$dbname = "solarDB";
-	$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+	
+	$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
+	
+	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	//This will display an error if connection fails
-	if(mysqli_connect_errno()) {
-		die("Database connection failed: " . 
-		mysqli_connect_error() . " ( " . mysqli_connect_errno() . ")"
-		);
-	}
+		return $dbConnection;
+}
 
-			// get the weather reading from the db
-	$query = 'SELECT weather FROM Monitor_Readings';
-	$result = mysqli_query($connection, $query) or die("mysql makes me cry " . mysqli_error($connection));
 
-	while ($row = mysqli_fetch_row($result)) 
-		$weather = $row[0];
+
+
+	?>
--- api/index.php
+++ api/index.php
@@ -1,4 +1,40 @@
 <?php
+require 'dbConnect.php';
+require 'Slim/Slim.php'
+
+\Slim\SLim::registerAutoLoader();
+
+$app = new Slim();
+
+
+//If the url matchs a certain word itll go to the function specified
+$app-> get('/readings','getReadings');
+
+//Invokes that said function I believe
+$app->run();
+
+
+function getReadings()
+{
+	$sql = 'SELECT * tables blah blahhh';
+	$db = getDb();
+	$stmt = $db -> query($sql);
+
+	$readings = $stmt->fetchAll(PDO::FETCH_OBJ);
+	$db = null;
+	echo '{"Readings": ' . json_encode($readings) . '}';
+} 
+	catch(PDOException $e) 
+	{
+	//error_log($e->getMessage(), 3, '/var/tmp/phperror.log'); //Write error log
+	echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+
+
+
+
+
+}
 
 
 

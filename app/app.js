c2b5bb70a941b7ace210224382098717e52cc8b7
sethChampagne <seth.champagne@selu.edu>
2015-03-17 22:02:06 -0500

    Added the routes

--- app/app.js
+++ app/app.js
@@ -9,6 +9,21 @@ var app = angular.module('dashboard', ['ui.router'])
         templateUrl: 'views/test.html',
         controller: 'HomeCtrl'
     })
+    .state('solar', {
+        url: '/solar',
+        templateUrl: 'views/solar.html',
+        controller: 'SolarCtrl'
+    })
+    .state('photo', {
+        url: '/photo',
+        templateUrl: 'views/photo.html',
+        controller: 'PhotoCtrl'
+    })
+    .state('geo', {
+        url: '/geo',
+        templateUrl: 'views/geo.html',
+        controller: 'GeoCtrl'
+    })
     .state('contact', {
         url: '/contact',
         templateUrl: 'views/contact.html'
